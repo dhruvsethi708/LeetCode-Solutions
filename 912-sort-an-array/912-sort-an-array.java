fd63a3be1e51edf55c3c059932ae462cd7ee5d5c
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-08-21 21:22:07 +0530

    Time: 19 ms (53.77%), Space: 52 MB (89.28%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,7 +1,59 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        // return Arrays.sort(nums);
-        Arrays.sort(nums);
+        mergesort(nums, 0, nums.length);
         return nums;
+    }
+    
+    public static void mergesort(int[] arr, int s, int e){
+            if (e-s==1){
+                return;
+            }
+
+        
+            int m = (s+e)/2;
+
+            mergesort(arr, s, m);
+            mergesort(arr, m, e);
+            
+            combine(arr, s, m, e);
+        
+    }
+    
+    public static void combine(int[] arr, int s, int m, int e){
+        int mix[] = new int[e-s];
+
+        
+        int i = s;
+        int j = m;
+        int k = 0;
+        
+        
+        while(i<m && j<e){
+            if(arr[i]>arr[j]){
+                mix[k] = arr[j];
+                j++;
+            }else{
+                mix[k] = arr[i];
+                i++;
+            }
+            k++;
+        }
+        
+        while(i<m){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        
+        while(j<e){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        
+        for(int l=0; l<mix.length; l++) { 
+			arr[s+l] = mix[l]; 
+		}
+        
     }
 }
