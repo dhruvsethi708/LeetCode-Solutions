2a7d65d09b34277f3a9b0ab5eeb0d35e9bf58625
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-08-24 17:33:27 +0530

    Time: 51 ms (15.13%), Space: 55.6 MB (8.72%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+class Solution {
+    public int dayOfYear(String date) {
+//         int[] arr = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+            
+        
+//         String mon = date.substring(5,7);
+//         int month = Integer.parseInt(mon);
+        
+//         String dat = date.substring(8,10);
+//         int din = Integer.parseInt(dat);
+        
+//         int day = din;
+        
+//         for(int i = 1; i<month; i++){
+//             day += arr[i-1]; 
+//         }
+
+//         String yr = date.substring(0,4);
+//         int year = Integer.parseInt(yr);
+        
+//         if(year%4==0 && year%100!=0 && year%400==0 && month>2){
+//             day = day +1;
+//         }
+        
+//         return day;
+        
+        return LocalDate.parse(date).getDayOfYear();
+    }
+}
