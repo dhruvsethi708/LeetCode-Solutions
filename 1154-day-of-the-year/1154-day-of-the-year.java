3dc4fe872e26de6431633cb2583c0def4c79195c
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-08-24 17:41:05 +0530

    Time: 19 ms (69.23%), Space: 52.7 MB (67.44%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,30 +1,36 @@
-import java.time.LocalDate;
+
 class Solution {
     public int dayOfYear(String date) {
-//         int[] arr = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+        int[] arr = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
             
         
-//         String mon = date.substring(5,7);
-//         int month = Integer.parseInt(mon);
+        String mon = date.substring(5,7);
+        int month = Integer.parseInt(mon);
         
-//         String dat = date.substring(8,10);
-//         int din = Integer.parseInt(dat);
+        String dat = date.substring(8,10);
+        int din = Integer.parseInt(dat);
         
-//         int day = din;
+        int day = din;
         
-//         for(int i = 1; i<month; i++){
-//             day += arr[i-1]; 
-//         }
+        for(int i = 1; i<month; i++){
+            day += arr[i-1]; 
+        }
 
-//         String yr = date.substring(0,4);
-//         int year = Integer.parseInt(yr);
+        String yr = date.substring(0,4);
+        int year = Integer.parseInt(yr);
         
-//         if(year%4==0 && year%100!=0 && year%400==0 && month>2){
-//             day = day +1;
-//         }
+        if(year%4==0 && month>2){
+            if(year%400==0){
+            day = day +1;                
+            }else if(year%100==0){
+                day = day;
+            }else{
+                day = day+1;
+            }
+            
+        }
         
-//         return day;
+        return day;
         
-        return LocalDate.parse(date).getDayOfYear();
     }
 }
