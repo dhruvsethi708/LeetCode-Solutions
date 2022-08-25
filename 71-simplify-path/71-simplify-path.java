c4a3cb424f718f10c689fa8c929ce573f1bb1b21
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-08-25 20:21:43 +0530

    Time: 9 ms (51.57%), Space: 43.6 MB (74.29%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        String[] p = path.split("/");
+        
+        for(int i = 0; i<p.length; i++){
+            if(!st.isEmpty() && p[i].equals("..")){
+                st.pop();
+            }else if(p[i]!="" && !p[i].equals("..") && !p[i].equals(".")){
+                st.push(p[i]);
+            }
+        }
+        
+        if(st.isEmpty()){
+            return "/";
+        }
+        
+        while(!st.isEmpty()){
+            res.insert(0,st.pop()).insert(0,"/");
+        }
+        
+        return res.toString();
+    }
+}
