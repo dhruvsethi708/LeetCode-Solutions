d763f925db112dab1788f932d0515094ecd177a2
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-09-05 18:01:01 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (40.18%) - LeetHub

--- 796-rotate-string/796-rotate-string.java
+++ 796-rotate-string/796-rotate-string.java
@@ -1,22 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
-            return false;
+        if(s.length()==goal.length() && (s+s).contains(goal)){
+            return true;
         }
-        if(s.equals("abcde") && goal.equals("abced")){
-            return false;
-        }
-        char[] sch = s.toCharArray();
-        Arrays.sort(sch);
         
-        char[] goalch = goal.toCharArray();
-        Arrays.sort(goalch);
-
-        for(int i = 0; i<sch.length; i++){
-            if(sch[i]!=goalch[i]){
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 }
