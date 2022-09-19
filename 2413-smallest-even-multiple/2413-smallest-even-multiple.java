f64dd04d494ff667618c6a0bc57a917d3682d599
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-09-19 17:02:24 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (60.00%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,13 +1,18 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
         
-        int gcf = 1;
+        //hcf is highest common factor of 2 and n.
+        int hcf = 1;
+        
+        // Calculating hcf
         for(int i = 1; i<=2 && i<=n; i++){
             if(2%i==0 && n%i==0){
-                gcf = i;
+                hcf = i;
             }
         }
         
-        return (2*n)/gcf;
+        // LCM of 2 numbers a and b is (a*b)/hcf(a,b)
+        
+        return (2*n)/hcf;
     }
 }
