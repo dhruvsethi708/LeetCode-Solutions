ad1a481388d0fd5bb63b075b1300d771f92937a9
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-09-25 18:06:41 +0530

    Time: 197 ms (44.44%), Space: 42 MB (64.49%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[0].length; j++){
+                boolean[][] visited = new boolean[board.length][board[0].length];
+                
+                if(board[i][j]==word.charAt(0) && findstring(board, i, j, visited, word, 0)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+        
+    }
+    
+    boolean findstring(char[][] board, int i, int j, boolean[][] visited, String word, int idx){
+        
+        if(idx>word.length()-1){
+            return true;
+        }
+        
+        if(i<0 || i>board.length-1 || j<0 || j>board[i].length-1 || board[i][j]!=word.charAt(idx) || visited[i][j] || word.length()>board.length*board[i].length){
+            return false;
+        }
+        
+        visited[i][j] = true;
+        if(findstring(board, i-1, j, visited, word, idx+1)){
+            return true;
+        }
+        if(findstring(board, i+1, j, visited, word, idx+1)){
+            return true;
+        }
+        if(findstring(board, i, j-1, visited, word, idx+1)){
+            return true;
+        }
+        if(findstring(board, i, j+1, visited, word, idx+1)){
+            return true;
+        }
+        
+        visited[i][j] = false;
+        
+        return false;
+    }
+}
