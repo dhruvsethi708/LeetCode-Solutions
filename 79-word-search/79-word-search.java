99d445ab9955af1d65f9b57d1cbc7f10ffe3d591
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-09-25 18:18:55 +0530

    Time: 126 ms (84.87%), Space: 40.1 MB (97.24%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
         for(int i = 0; i<board.length; i++){
             for(int j = 0; j<board[0].length; j++){
-                boolean[][] visited = new boolean[board.length][board[0].length];
                 
                 if(board[i][j]==word.charAt(0) && findstring(board, i, j, visited, word, 0)){
                     return true;
