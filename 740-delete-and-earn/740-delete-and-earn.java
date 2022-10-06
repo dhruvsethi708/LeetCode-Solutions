d5d84dd4c1afe556362e6081a8cf661f6261a230
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-10-07 00:43:50 +0530

    Time: 2 ms (99.08%), Space: 42.5 MB (95.66%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        
+        int[] dp = new int[10001];
+        
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            max = Math.max(nums[i], max);
+            dp[nums[i]] += nums[i]; 
+        }
+        
+        for(int i = 2; i<=max; i++){
+            dp[i] = Math.max(dp[i] + dp[i-2], dp[i-1]);
+        }
+        
+        return Math.max(dp[max], dp[max-1]);
+        
+    }
+}
