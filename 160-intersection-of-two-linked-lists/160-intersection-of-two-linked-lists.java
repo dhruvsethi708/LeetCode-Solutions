6efcbb0dbf46a6778ab85b0619c70064da214958
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-10-08 00:08:27 +0530

    Time: 8 ms (33.54%), Space: 55.1 MB (65.51%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        //Floyd Cycle Method
+        
+        if(headA==null || headB==null){
+            return null;
+        }
+        
+        ListNode tail = headA;
+        while(tail.next!=null){
+            tail = tail.next;
+        }
+        
+        tail.next = headB;
+        
+        ListNode ans = startingNodeOfCycle(headA);
+        
+        tail.next = null;
+        
+        return ans;
+
+    }
+    
+    ListNode startingNodeOfCycle(ListNode head){
+        
+        if(head==null || head.next==null){
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow==fast){
+                break;
+            }
+        }
+        
+        if(slow!=fast){
+            return null;
+        }
+        
+        slow = head;
+        
+        while(slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return slow;
+    }
+}
