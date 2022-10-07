0327df3cc5cee5172de48c543516fbe5c18cd2e6
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-10-08 00:30:15 +0530

    Time: 2 ms (35.09%), Space: 45.8 MB (49.19%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -12,10 +12,6 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
-        if(head==null || head.next==null){
-            return false;
-        }
-        
         ListNode slow = head;
         ListNode fast = head;
         
