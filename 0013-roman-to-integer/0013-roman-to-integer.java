ee12ab1e2697d82c936299cad9e929d591ebaf02
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-10-24 00:35:52 +0530

    Time: 13 ms (38.41%), Space: 47.2 MB (19.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,62 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
         
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
         int ans = 0;
         
-        for(int i = s.length()-1; i>=0; i--){
+        ans += map.get(s.charAt(s.length()-1));
+        
+        for(int i = s.length()-2; i>=0; i--){
             
-            if(i-1>=0 && s.charAt(i)=='V' && s.charAt(i-1)=='I'){
-                ans+=4;
-                i--;
-                continue;
-            }
-            else if(i-1>=0 && s.charAt(i)=='X' && s.charAt(i-1)=='I'){
-                ans+=9;
-                i--;
-                continue;
-            }
-            else if(i-1>=0 && s.charAt(i)=='L' && s.charAt(i-1)=='X'){
-                ans+=40;
-                i--;
-                continue;
-            }
-            else if(i-1>=0 && s.charAt(i)=='C' && s.charAt(i-1)=='X'){
-                ans+=90;
-                i--;
-                continue;
-            }
-            else if(i-1>=0 && s.charAt(i)=='D' && s.charAt(i-1)=='C'){
-                ans+=400;
-                i--;
-                continue;
-            }
-            else if(i-1>=0 && s.charAt(i)=='M' && s.charAt(i-1)=='C'){
-                ans+=900;
-                i--;
-                continue;
-            }
-            
-            
-            if(i>=0){
-                
-                if(s.charAt(i)=='I'){
-                    ans+=1;
-                }else if(s.charAt(i)=='V'){
-                    ans+=5;
-                }else if(s.charAt(i)=='X'){
-                    ans+=10;
-                }else if(s.charAt(i)=='L'){
-                    ans+=50;
-                }else if(s.charAt(i)=='C'){
-                    ans+=100;
-                }else if(s.charAt(i)=='D'){
-                    ans+=500;
-                }else if(s.charAt(i)=='M'){
-                    ans+=1000;
-                }
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                ans -= map.get(s.charAt(i));
+            }else{
+                ans += map.get(s.charAt(i));
             }
         }
-        
+
         return ans;
     }
 }
