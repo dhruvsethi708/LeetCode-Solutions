bad7d5e1fe0cb15d34f4841c965dd2040f0d6c51
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2022-11-04 13:03:43 +0530

    Time: 1696 ms (6.20%), Space: 681.7 MB (5.01%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -3,16 +3,22 @@ class Solution {
         
         StringBuilder sb = new StringBuilder();
         
+        // Storing all the vowels in the word in stringbuilder
         for(int i = 0; i<s.length(); i++){
             if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
                 sb.append(s.charAt(i));
             }
         }
         
+        // reversing the stringbuilder
         sb.reverse();
         
         int idx = 0;
         String ans = "";
+        
+        // traversing through the word again
+        //If the character is vowel adding from stringbuilder in the ans string 
+        //else adding the character of the word
         
         for(int i = 0; i<s.length(); i++){
             if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
