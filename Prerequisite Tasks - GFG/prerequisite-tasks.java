df7061205c6a52bf9054a523cc95a32832bdabb1
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2023-01-09 23:42:53 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main(String args[]) throws IOException
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+		while(t>0)
+		{
+		    int N = sc.nextInt();
+		    int P = sc.nextInt();
+		    int prerequisites[][] = new int[P][2];
+		    for(int i=0;i<P;i++)
+		    {
+		        for(int j=0;j<2;j++)
+		        {
+		            prerequisites[i][j] = sc.nextInt();
+		        }
+		    }
+			Solution ob = new Solution();
+			if(ob.isPossible(N,prerequisites))
+			{
+			    System.out.println("Yes");
+			}
+			else{
+			    System.out.println("No");
+			}
+			t--;
+		}
+	}
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public boolean isPossible(int n, int[][] prerequisites)
+    {
+        // Your Code goes here
+        
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int m = prerequisites.length;
+
+        for(int i = 0; i<m; i++){
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+        int indegree[] = new int[n];
+        for(int i = 0; i<n; i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i<n; i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+
+            topo.add(node);
+
+            for(int i: adj.get(node)){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.add(i);
+                }
+            }
+        }
+
+        if(topo.size()==n) return true;
+
+        return false;
+    }
+    
+}
