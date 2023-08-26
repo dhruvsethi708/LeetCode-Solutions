9256b026f901b8d35f956094526cd62e1a706733
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2023-08-26 23:09:35 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	  
+	  //taking input using Scanner class
+	  Scanner sc = new Scanner(System.in);
+	  
+	  //taking count of testcases
+	  int t = sc.nextInt();
+	  while(t-- > 0){
+	      
+	      //taking count of houses
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      
+	      //inserting money present in 
+	      //each house in the array
+	      for(int i = 0; i<n; ++i)
+	           arr[i] = sc.nextInt();
+  	      
+  	      //calling method FindMaxSum() of class solve
+  	      System.out.println(new Solution().FindMaxSum(arr, n));
+	 }
+   }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    int[] dp;
+    
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        
+        dp = new int[n+1];
+        
+        int ans = helper(arr, n-1);
+        
+        return ans;
+    }
+    
+    int helper(int[] arr, int n){
+        
+        if(n<0) return 0;
+        
+        if(n == 0) return arr[0];
+        
+        if(dp[n] != 0) return dp[n];
+        
+        int include = arr[n] + helper(arr, n-2);
+        int exclude = helper(arr, n-1);
+        
+        dp[n] = Math.max(include, exclude);
+        return Math.max(include, exclude);
+    }
+}
