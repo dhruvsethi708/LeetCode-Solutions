1691fb3e184d010bc73a3ef8c14e01d0c17755c1
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2023-12-21 23:36:43 +0530

    Added solution - LeetHub

---
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GfG { 
+    
+	public static void main (String[] args) throws IOException  {
+	    
+	    //reading input using BufferedReader class
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//reading total testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    
+		    //reading number of eggs and floors
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    int n = Integer.parseInt(inputLine[0]);
+		    int k = Integer.parseInt(inputLine[1]);
+		    
+		    //calling eggDrop() method of class
+		    //EggDrop
+		    System.out.println(new Solution().eggDrop(n, k));
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    static int eggDrop(int n, int k) 
+	{
+	    // Your code here
+	    
+	    int[][] dp = new int[n+1][k+1];
+        int m = 0;
+        
+        while(dp[n][m] < k){
+            m+=1;
+            
+            for(int i=1; i<=n; i++){
+                dp[i][m] = dp[i][m-1] + dp[i-1][m-1] + 1;
+            }
+        }
+        
+        return m;
+	}
+}
