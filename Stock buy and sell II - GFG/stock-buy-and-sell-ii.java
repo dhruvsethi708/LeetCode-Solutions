b4c716e4991fbd866e95964f07e7aa7ca3b08e0d
Dhruv Sethi <74965209+dhruvsethi708@users.noreply.github.com>
2023-12-23 14:57:48 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        // code here
+        
+        int ans = 0;
+        
+        for(int i = 1; i<n; i++){
+            if(prices[i] > prices[i-1]){
+                ans += prices[i] - prices[i-1];
+            }
+        }
+        
+        return ans;
+    }
+    
+    // static int helper(int n, int[] prices, int i, int buy){
+    //     if(i>=n) return 0;
+        
+    //     int profit = 0;
+        
+    //     if(buy == 1){
+    //       profit = Math.max((-arr[i] + Stock(n,arr,i+1,0,dp)), (0 + Stock(n,arr,i+1,1,dp))); 
+    //     }else{
+    //       profit = Math.max((arr[i] + Stock(n,arr,i+1,1,dp)), (0 + Stock(n,arr,i+1,0,dp))); 
+    //     }
+    // }
+}
+        
